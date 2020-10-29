83b5fd99e9f2779bbd925aa982a78d8b34bb1e0a
tdaroly <taherhuzefa1998@gmail.com>
2020-10-28 22:10:06 -0500

    changes to see if pushing fixes deploymenet error

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-
-
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Topbar extends Component{
 
@@ -10,7 +10,19 @@ class Topbar extends Component{
 
 
         return(
-            <div> Topbar</div>
+                <div>
+                        
+                        <Container fluid style = {{backgroundColor:'#f7b7f6'}}>
+                                    
+                        <Row>
+                            <Col>Choose Algorithm</Col>
+                            <Col># Of bars</Col>
+                            <Col>Randomize Button</Col>
+                        </Row>
+                        </Container>
+                </div>
+            
+            
         )
     }
 }
