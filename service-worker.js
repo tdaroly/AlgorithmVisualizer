76df90422d3d33b0228765191f5cf2772c624d19
tdaroly <taherhuzefa1998@gmail.com>
2020-10-28 22:10:36 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AlgorithmVisualizer/precache-manifest.910e20e5083f44bf4e18f2552acfb286.js"
+  "/AlgorithmVisualizer/precache-manifest.2bdaa0c89be8ac792a8f4d82a03a9070.js"
 );
 
 self.addEventListener('message', (event) => {
