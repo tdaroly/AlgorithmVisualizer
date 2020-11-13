13bdc09ade0435addf0bab989eaac75c8704b8a8
tdaroly <taherhuzefa1998@gmail.com>
2020-11-12 23:04:38 -0600

    added slider

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -4,12 +4,21 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
 import '../../App.css';
+import ReactBootstrapSlider from 'react-bootstrap-slider';
 
-const sortingMethods = ["Bubble Sort","Merge Sort", "Insertion Sort","Selection Sort","HeapSort","QuickSort"];
-const dropdownitems = sortingMethods.map((sorts) =>
-<Dropdown.Item >{sorts}</Dropdown.Item>
-)
+
+
+const sortingMethods = [
+    "Bubble Sort",
+    "Merge Sort",
+    "Insertion Sort",
+    "Selection Sort",
+    "HeapSort",
+    "QuickSort"
+];
+const dropdownitems = sortingMethods.map((sorts) => <Dropdown.Item >{sorts}</Dropdown.Item>)
 class Bottom extends Component {
 
     render()
@@ -20,10 +29,11 @@ class Bottom extends Component {
                 <Container fluid={true}>
                     <h1 className='title-text'>
                         SORTING VISUALIZER</h1>
-                    <Row className = "text-center">
+                    <Row className="text-center">
                         <Col >
+                            <h6>Select Algorithm</h6>
                             <Dropdown>
-                                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
                                     Dropdown Button
                                 </Dropdown.Toggle>
 
@@ -32,8 +42,16 @@ class Bottom extends Component {
                                 </Dropdown.Menu>
                             </Dropdown>
                         </Col>
-                        <Col ># Of bars</Col>
-                        <Col >Randomize Button</Col>
+                        <Col >
+                            <h6># Of bars</h6>
+                            <input type="range" id="volume" name="volume"
+         min="0" max="11"/>
+                            <h6># on top goes here</h6>
+                        </Col>
+                        <Col >
+                            <h6>Randomize</h6>
+                                <Button variant="primary">Primary</Button>{' '}
+                        </Col>
 
                     </Row>
                 </Container>
